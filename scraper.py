1d1750daa817d6dc5114e2b4d3bed0a0522dc69e
vlakos <vkostic86@gmail.com>
2018-08-01 22:28:41 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import urllib2
 import lxml.etree
 
 #createavariablecalled'url'andthenreadwhat'sthere
-url="http://www.staffssaferroads.co.uk/media/114997/03092012_forwebsite.pdf"
+url="http://www.acas.rs/wp-content/uploads/2017/12/Godisnji-plan-provere-za-2018.pdf"
 pdfdata = urllib2.urlopen(url).read()
 print "The pdf file has %d bytes" % len(pdfdata)
 
@@ -14,12 +14,12 @@ print "After converting to xml it has %d bytes" % len(xmldata)
 root = lxml.etree.fromstring(xmldata)
 
 #thislineusesxpathtofind<text>tags
-lines = root.findall('.//text[@font="5"]')
-print lines
-for line in lines:
-    print line.text
+#lines = root.findall('.//text[@font="5"]')
+#print lines
+#for line in lines:
+    #print line.text
 
-record = {} 
-for line in lines:
-    record["date"] = line.text
-    scraperwiki.sqlite.save(['date'], record)
+#record = {} 
+#for line in lines:
+    #record["date"] = line.text
+    #scraperwiki.sqlite.save(['date'], record)
