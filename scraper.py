280c52305b38d48fb4f1288e754e05976e50bf25
vlakos <vkostic86@gmail.com>
2018-08-01 22:36:49 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,25 +1,36 @@
-#importourlibraries
 import scraperwiki
 import urllib2
 import lxml.etree
 
-#createavariablecalled'url'andthenreadwhat'sthere
-url="http://www.acas.rs/wp-content/uploads/2017/12/Godisnji-plan-provere-za-2018.pdf"
-pdfdata = urllib2.urlopen(url).read()
-print "The pdf file has %d bytes" % len(pdfdata)
-
-#converttoxmlandprintsomeinfo
+url="https://www.parliament.uk/documents/lords-committees/house/Minutes/2015-16/HCMinute\s-6-030516.pdf"
+pdfdata=urllib2.urlopen(url).read()
+print "Thepdffilehas%dbytes"%len(pdfdata)
 xmldata = scraperwiki.pdftoxml(pdfdata)
 print "After converting to xml it has %d bytes" % len(xmldata)
+print "The first 2000 characters are:", xmldata[:2000]
 root = lxml.etree.fromstring(xmldata)
-
-#thislineusesxpathtofind<text>tags
-#lines = root.findall('.//text[@font="5"]')
-#print lines
-#for line in lines:
-    #print line.text
-
-#record = {} 
-#for line in lines:
-    #record["date"] = line.text
-    #scraperwiki.sqlite.save(['date'], record)
+pages = list(root)
+print "The pages are numbered:", [ page.attrib.get("number") for page in pages ]
+#thisfunctionhastoworkrecursivelybecausewemighthave"<bPart1<ipart2</i</b"
+def gettext_with_bi_tags(el):
+res = [ ]
+if el.text:
+res.append(el.text)
+Scraper #22: Scraping PDFs part 1 190
+for lel in el:
+    res.append("<%s" % lel.tag) res.append(gettext_with_bi_tags(lel)) res.append("</%s" % lel.tag)
+if el.tail:
+    return "".join(res) 35
+#printthefirsthundredtextelementsfromthefirstpage
+page0=pages[0]
+ID=0
+for el in list(page0)[:100]:
+    print el.tag
+if el.tag == "text":
+    print el.attrib, gettext_with_bi_tags(el)
+record = {}
+record["text"] = gettext_with_bi_tags(el)
+ID = ID+1
+record["ID"] = ID
+scraperwiki.sqlite.save(["ID"],record)
+print record
